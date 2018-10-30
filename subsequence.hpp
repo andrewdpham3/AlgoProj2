fdeb05a6339a3d05a11c544947b8474068a15f7f
andrewdpham3 <andrewdpham3@gmail.com>
2018-10-29 21:08:35 -0700

    Update subsequence.hpp

--- subsequence.hpp
+++ subsequence.hpp
@@ -78,8 +78,8 @@ sequence longest_increasing_end_to_beginning(const sequence& A) {
         // write the statements that compute the value of
         // H[i] based on conditions that involve A[i], A[j]x
         // and H[j]
-      if(A[j]>A[i] && H[j]>H[i])
-        H[i]=H[j]+1;
+      if(A[j]>A[i] && A[j]>H[i])
+        H[i+1]=A[j];
     }
   }
 
