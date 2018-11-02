8cd4c012172d867e6636a48075825e28bfe6f195
andrewdpham3 <andrewdpham3@gmail.com>
2018-11-01 20:57:14 -0700

    Update subsequence.hpp

--- subsequence.hpp
+++ subsequence.hpp
@@ -85,13 +85,6 @@ sequence longest_increasing_end_to_beginning(const sequence& A) {
     }
   }
   
-  cout<<"\n";
-  for(int i=0;i<n-1;i++)
-    cout<<A[i]<<",";
-  cout<<"\n";
-  for(int i=0;i<n-1;i++)
-    cout<<H[i]<<",";
-  
   // calculate in max the length of the longest subsequence
     // by adding 1 to the maximum value in H
   auto max = *std::max_element(H.begin(), H.end()) + 1;
