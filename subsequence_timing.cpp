b5c45bbb62b40e154926e02875064a9708d5a5ea
andrewdpham3 <andrewdpham3@gmail.com>
2018-11-01 22:08:32 -0700

    Update subsequence_timing.cpp

--- subsequence_timing.cpp
+++ subsequence_timing.cpp
@@ -54,10 +54,10 @@ void multitest(size_t n){
 }
 
 int main() {
-  multitest(6);
-  multitest(12);
-  multitest(18);
-  multitest(24);
-  multitest(30);
+  multitest(5);
+  multitest(10);
+  multitest(15);
+  multitest(20);
+  multitest(25);
   return 0;
 }
